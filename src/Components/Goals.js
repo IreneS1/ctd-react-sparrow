4effe8dbdb03ba26c0bee85e8622b23fc44567af
Irene Sanchez <irene.iscc777@gmail.com>
2022-07-14 13:21:34 -0700

    added goals page

---
+++ src/Components/Goals.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import Airtable from 'airtable';
+import AddTodoForm from './AddTodoForm';
+import Navbar from './Navbar';
+import TodoList from './TodoList';
+
+const base = new Airtable({ apiKey: process.env.REACT_APP_AIRTABLE_API_KEY }).base(
+    process.env.REACT_APP_AIRTABLE_BASE_ID
+);
+
+const Goals = () => {
+
+    const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem("savedTodoList")));
+    const [isLoading, setIsLoading] = React.useState(true);
+
+    // GET request with prop of table name
+    React.useEffect(() => {
+        base('goals')
+            .select({ view: "Grid view" })
+            .eachPage((records, fetchNextPage) => {
+                setTodoList(records);
+                console.log("The records from GET", records);
+                setIsLoading(false);
+                fetchNextPage();
+            });
+    }, []);
+
+
+    // useEffect on loading
+    React.useEffect(() => {
+        if (!isLoading) {
+            localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+        }
+    });
+
+    // add to do using Create()
+    const addTodo = (newTodo) => {
+        base('default').create({
+            "title": newTodo.title,
+            "priority": "fasle"
+        }, { typecast: true }, function (err, record) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(record.fields.title);
+            setTodoList([...todoList, record])
+
+        });
+    }
+
+    // API call to DELETE todo list item
+    const removeTodo = (id) => {
+        const item = todoList.find(element => element.id === id);
+        base('default').destroy(item.id) // destroy using element id
+        console.log("This is the item removed", item);
+        const itemIndex = todoList.indexOf(item);
+        todoList.splice(itemIndex, 1);
+        const updatedTodoList = [].concat(todoList);
+        setTodoList(updatedTodoList);
+    }
+
+    // API call to update todo list item
+    const updateTodo = (updatedTodo, id) => {
+        console.log("update mode active", updatedTodo);
+        console.log(updatedTodo, id)
+        const item = todoList.find(element => element.id === id);
+        base('default').update(id, {
+            "priority": "fasle",
+            "title": updatedTodo
+        }, function (err, record) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            const itemIndex = todoList.indexOf(item);
+            todoList[itemIndex] = updatedTodo;
+
+            console.log(todoList)
+        });
+    }
+
+    return (
+        <div className='todo-app'>
+            <Navbar />
+            <div className='container'>
+                <h1 className='header1'>Goals</h1>
+                <hr />
+                <div className='mainContainer'>
+                    <AddTodoForm onAddTodo={addTodo} />
+                    {isLoading ? <p>Loading...</p> :
+                        <TodoList
+                            todoList={todoList}
+                            onRemoveTodo={removeTodo}
+                            onUpdateTodo={updateTodo} />}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Goals
